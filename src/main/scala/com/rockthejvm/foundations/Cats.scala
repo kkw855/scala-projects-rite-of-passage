046f4b98b5b4474c618a52f2ad8a3fd6759eb5d6
kkw855 <kkw855@gmail.com>
2023-09-27 22:23:01 +0900

    🐛 fix: scalafmt 설정 파일 이름 고치기

---
+++ src/main/scala/com/rockthejvm/foundations/Cats.scala
@@ -0,0 +1,16 @@
+package com.rockthejvm.foundations
+
+object Cats {
+  /*
+    type classes
+    - Applicative
+    - Functor
+    - FlatMap
+    - Monad
+    - ApplicativeError/MonadError
+   */
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
